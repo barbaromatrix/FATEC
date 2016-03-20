22a525e3d1089917f13e107ccc022c947598e7ad
barbaromatrix <matheus.galdino@hotmail.com>
2016-03-19 21:16:19 -0300

    College's project - Applied Statistic

---
+++ estatistica/src/index/Media.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package index;
+
+/**
+ *
+ * @author barbaromatrix
+ */
+public class Media {
+    private int quantidadeElementos;
+    
+    public Media(int quantidadeElementos){
+        this.quantidadeElementos = quantidadeElementos;
+    }
+    
+    public Media(){}
+    
+    public double media(){
+        return 0;
+    }
+    
+    public double arredondaNumero(double numero){
+        return 0;
+    }
+}
