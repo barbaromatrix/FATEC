37c6ad1433575b863a66dfdbd07396c671438955
barbaromatrix <matheus.galdino@hotmail.com>
2016-03-26 15:14:01 -0300

    Feito método para achar frequenciaAcumulada

--- estatistica/src/index/Media.java
+++ estatistica/src/index/Media.java
@@ -20,7 +20,7 @@ public class Media {
      */
     public double media(double[] elementos)
     {
-        double soma = soma(elementos, elementos.length - 1);
+        double soma = soma(elementos, elementos.length - 1, 0);
         return soma / elementos.length;
     }
     
@@ -37,7 +37,7 @@ public class Media {
             soma += (elementos[i] * frequencia[i]);
         }
         
-        return soma/soma(frequencia, frequencia.length - 1);
+        return soma/soma(frequencia, frequencia.length - 1, 0);
     }
     
     /**
@@ -50,29 +50,85 @@ public class Media {
     {
         double soma = 0, somaParcial = 0;
         for (int i = 0; i < elementos.length; i++) {
-            somaParcial = (soma(elementos[i], elementos[i].length - 1)/2) * frequencia[i];
+            somaParcial = (soma(elementos[i], elementos[i].length - 1, 0)/2) * frequencia[i];
             soma += somaParcial;
         }
         
-        double media = soma / soma(frequencia, frequencia.length - 1);
+        double media = soma / soma(frequencia, frequencia.length - 1, 0);
         
         return media;
     }
+        
+    /**
+     * 
+     * @param elementos
+     * @return A mediana simples de elementos sem intervalo de classe
+     */
+    public double mediana(double[] elementos)
+    {
+        return (elementos.length % 2 == 0) ? ((elementos[(elementos.length / 2) - 1] + elementos[(elementos.length/2)])/2) : elementos[elementos.length / 2];
+    }
     
+    /**
+     * 
+     * @param elementos
+     * @param frequencia
+     * @return a moda de um conjunto de elementos
+     */
+    public double moda(double[] elementos, double[] frequencia)
+    {
+        double max = 0;
+        int place = 0;
+        for (int i = 0; i < frequencia.length; i++) 
+        {
+            if(frequencia[i] > max)
+            {
+                max = frequencia[i];
+                place = i;
+            }
+        }
+        return elementos[place];
+    }
+    
+    /**
+     * 
+     * @param elementos
+     * @param frequencia
+     * @return mediana de um intervalo de classe de x elementos
+     */
+    public double mediana(double[][] elementos, double[] frequencia)
+    {  
     /*
     fórmula da mediana = limite inferior + ((((0,5 * sumFrequencia) - frequenciaAcumuladaAcima) * h)/ frequenciaDaClasse)
     */
+        
+        double media = media(elementos, frequencia);
+        return media;
+    }
     
     /**
      * 
      * @param f
      * @param controle
-     * @return soma recursiva do vetor f
+     * @return soma recursiva do vetor f que soma todo o vetor
      */
     public double soma(double[] f, int controle)
     {
         if(controle == 0) return f[0];
         
         return f[controle] + soma(f, controle - 1);
+    }
+    /**
+     * 
+     * @param f
+     * @param controle => Elemento limite de comparação => escape da recursividade
+     * @param passagem => Elemento x do vetor que é incrementado
+     * @return Uma soma qualquer com limite
+     */
+    public double soma(double[] f, int controle, int passagem)
+    {
+        if (passagem == controle) return f[passagem];
+        
+        return soma(f, controle, passagem + 1) + f[passagem];
     }
 }
--- estatistica/src/index/index.java
+++ estatistica/src/index/index.java
@@ -5,6 +5,8 @@
  */
 package index;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author barbaromatrix
@@ -12,12 +14,10 @@ package index;
 public class index {
     public static void main(String[] args) {
         Media m = new Media();
-        double cenoura[] = {0, 1, 2, 3, 4, 5, 6, 7};
-        double frequencia[] = {45, 52, 48, 55, 30, 10, 8, 2};
+        /*double cenoura[] = {0, 1, 2, 3, 4, 5, 6, 7};
+        double frequencia[] = {45, 52, 48, 55, 30, 10, 8, 2};*/        
         
-        System.out.println(m.media(cenoura));
-        
-        /*String elementos[] = {"[180;200[", "[200;220[", "[220;240[", "[240;260[", "[260;280["};
+        String elementos[] = {"[180;200[", "[200;220[", "[220;240[", "[240;260[", "[260;280["};
         double[][] elementosSeparados = new double[elementos.length][2];
         String[] split;
         
@@ -30,6 +30,6 @@ public class index {
         }
         double[] frequencia = {4, 18, 10, 5, 3};
         System.out.println(m.media(elementosSeparados, frequencia));
-        */
+        
   }
 }
